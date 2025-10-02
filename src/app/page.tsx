5ed9154ff729825d6e12fe7a04f81f836f884efe
hussainmanasa <manasawala_hussain@yahoo.com>
2025-10-02 23:09:53 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <iframe
+      loading="lazy"
+      src="https://www.canva.com/design/DAG0m2LMMLc/lKljRJeXQg01VpSq0XnEgw/watch?embed"
+      allowFullScreen
+      width="100%"
+      height="100%"
+    ></iframe>
+  );
+}
